9ed12206a7a332737b08e61519b0a72fe8b74244
dominicod <dominicodonnell99@gmail.com>
2022-09-15 20:19:23 -0500

    Test: test created for Admin Merchants Index, Feat: created logic for Admin Merchants Index

---
+++ spec/features/admin/merchants/index_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'As an admin,' do
+  describe 'When I visit the admin Merchants index ("/admin/merchants")' do
+    it 'Then I see the name of each merchant in the system' do
+      visit admin_merchants_path
+
+      within("#merchant_names") do
+        merchant_ids = (1..30).to_a
+
+        expect(page).to have_content("Schroeder-Jerde")
+        expect(page).to have_content("Pollich and Koelpin")
+        expect(page).to have_content("Jones and Stokes")
+        expect(page).to have_content("Ernser, Borer and Marks")
+
+        merchant_ids.each {|id| expect(page.has_css?("##{id}")).to eq true}
+      end
+    end
+  end
+end
