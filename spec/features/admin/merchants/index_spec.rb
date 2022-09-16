aac7e55d7ca1dbf5482356f7ec61378001534bfc
dominicod <dominicodonnell99@gmail.com>
2022-09-15 20:46:08 -0500

    Test: create test for admin show, Feat: create route and controller show method

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -8,17 +8,29 @@ RSpec.describe 'As an admin,' do
       within("#merchant_names") do
         merchant_ids = (1..30).to_a
 
-        expect(page).to have_content("Schroeder-Jerde")
-        expect(page).to have_content("Pollich and Koelpin")
-        expect(page).to have_content("Jones and Stokes")
-        expect(page).to have_content("Ernser, Borer and Marks")
+        expect(page).to have_link("Schroeder-Jerde")
+        expect(page).to have_link("Pollich and Koelpin")
+        expect(page).to have_link("Jones and Stokes")
+        expect(page).to have_link("Ernser, Borer and Marks")
 
         merchant_ids.each {|id| expect(page.has_css?("##{id}")).to eq true}
       end
     end
 
     it "When I click on the name of a merchant from the admin merchants index page, I am taken to that merchant's admin show page (/admin/merchants/merchant_id)" do
+      visit admin_merchants_path
 
+      within("#merchant_names") do
+        expect(page).to have_link("Schroeder-Jerde")
+        click_link "Schroeder-Jerde"
+        expect(page.current_path).to eq admin_merchants_path("1")
+
+        visit admin_merchants_path
+
+        expect(page).to have_link("Ernser, Borer and Marks")
+        click_link "Ernser, Borer and Marks"
+        expect(page.current_path).to eq admin_merchants_path("30")
+      end
     end
   end
 end
