846b37233c6d36d45ccac81397479b70835b9e36
Thomas Hayes <hayes.thomas610@gmail.com>
2022-09-17 15:33:30 -0600

    User Story 12: add model tests

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -12,4 +12,26 @@ RSpec.describe Merchant, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:name) }
   end
+
+  describe 'instance methods' do
+    describe '.enabled_items' do
+      it 'returns a list of all the items that are enabled for a specific merchant' do
+        merchant = Merchant.first
+        expect(merchant.enabled_items.count).to eq(0)
+
+        merchant.items.first.update(status: "enabled")
+        expect(merchant.enabled_items.count).to eq(1)
+      end
+    end
+
+    describe '.disabled_items' do
+      it 'returns a list of all the items that are disabled for a specific merchant' do
+        merchant = Merchant.first
+        expect(merchant.disabled_items.count).to eq(3)
+
+        merchant.items.first.update(status: "enabled")
+        expect(merchant.disabled_items.count).to eq(2)
+      end
+    end
+  end
 end
