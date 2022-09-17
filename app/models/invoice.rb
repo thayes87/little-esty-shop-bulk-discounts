0f3a8ac65169e951691ef5b57d95c7280c0dacc7
Lucas Colwell <colwellc37@gmail.com>
2022-09-17 11:20:08 -0600

    Feat: add total revenue method to invoice model

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -11,4 +11,8 @@ class Invoice < ApplicationRecord
   def self.not_shipped_invoices
     joins(:invoice_items, :customer).where('invoice_items.status != 2').order(:created_at)
   end
+
+  def total_revenue
+    invoice_items.sum('invoice_items.quantity * invoice_items.unit_price')
+  end
 end
