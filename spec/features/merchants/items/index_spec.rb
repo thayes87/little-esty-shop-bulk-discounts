613630571da5530384bc712c23d1c870cb99fcdc
Thomas Hayes <hayes.thomas610@gmail.com>
2022-09-17 13:40:13 -0600

    test build out for story 12

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -6,18 +6,45 @@ RSpec.describe 'Merchant Items Index Page' do
       it 'I see a list of the names of all of my items' do
         visit merchant_items_path(Merchant.first)
 
-        expect(page).to have_content("Item Qui Esse")
-        expect(page).to have_content("Item Autem Minima")
-        expect(page).to have_content("Item Ea Voluptatum")
+          expect(page).to have_content("Item Qui Esse")
+          expect(page).to have_content("Item Autem Minima")
+          expect(page).to have_content("Item Ea Voluptatum")
       end
 
       it 'I do not see items for any other merchant' do
         visit merchant_items_path(Merchant.first)
 
-        expect(page).to_not have_content("Item Nemo Facere")
-        expect(page).to_not have_content("Item Expedita Aliquam")
-        expect(page).to_not have_content("Item Provident At")
-        expect(page).to_not have_content("Item Itaque Consequatur")
+          expect(page).to_not have_content("Item Nemo Facere")
+          expect(page).to_not have_content("Item Expedita Aliquam")
+          expect(page).to_not have_content("Item Provident At")
+          expect(page).to_not have_content("Item Itaque Consequatur")
+      end
+
+      it 'has two sections, one for "Enabled Items"  and one for "Disabled Items"' do 
+        visit merchant_items_path(Merchant.first)
+
+          expect(page).to have_content("Enabled Items")
+          expect(page).to have_content("Disabled Items")
+
+        within "div#Disabled_Items" do
+          expect(page).to have_content("Autem Minima")
+          expect(page).to have_content("Ea Voluptatum")
+          expect(page).to have_content("Item Qui Esse")
+        end
+
+        find("div#1").click_button("enable")
+
+        within "div#Enabled_Items" do
+          expect(page).to have_content("Item Qui Esse")
+          expect(page).to_not have_content("Ea Voluptatum")
+          expect(page).to_not have_content("Item Qui Esse")
+        end
+
+        within "div#Disabled_Items" do
+          expect(page).to have_content("Autem Minima")
+          expect(page).to have_content("Ea Voluptatum")
+          expect(page).to_not have_content("Item Qui Esse")
+        end
       end
     end
   end
