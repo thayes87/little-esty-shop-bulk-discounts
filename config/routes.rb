10670e5cf21141b21ce7f714d1baf826cc5d843b
Thomas Hayes <hayes.thomas610@gmail.com>
2022-09-18 12:12:19 -0600

    reorganize merchant items into merchant folder for views and spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  resources :merchants, module: :merchant do
+    resources :items
+    resources :invoices, only: [:index]
+  end
+
   resources :merchants do
     resources :dashboards, only: [:index, :create]
-    resources :items, only: [:index, :show, :edit, :update, :new, :create]
-    resources :invoices, only: [:index]
   end
 
   #get '/merchants/:id/items/:id', to: 'items#edit'
