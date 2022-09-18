d044de05c2dec40a9c7420e24e9919b234865f0a
dominicod <dominicodonnell99@gmail.com>
2022-09-18 10:19:09 -0500

    Feat: add :new and :create routes to config'

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   resources :admin, only: [:index]
   namespace :admin do
     resources :invoices, only: [:index, :show]
-    resources :merchants, only: [:index, :show, :edit, :update]
+    resources :merchants, only: [:index, :show, :edit, :update, :new, :create]
   end
 end
