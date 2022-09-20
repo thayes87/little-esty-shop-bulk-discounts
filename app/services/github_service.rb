76864873b9e93f4753438ca3940e4073f254fb54
Thomas Hayes <hayes.thomas610@gmail.com>
2022-09-20 16:19:01 -0600

    add collaborator info for github API repo

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -2,13 +2,13 @@ require 'httparty'
 require 'json'
 
 class GitHubService
-  def self.request(path, auth_required = false)
+  def self.request(path, auth_required)
     if auth_required
       response = HTTParty.get("https://api.github.com/repos/Dominicod/little-esty-shop#{path}", headers: {authorization: "Bearer " + ENV['token']})
-      json_response = JSON.parse(response.body, symbolize_names: true)
+      JSON.parse(response.body, symbolize_names: true)
     else
       response = HTTParty.get("https://api.github.com/repos/Dominicod/little-esty-shop#{path}")
-      json_response = JSON.parse(response.body, symbolize_names: true)
+      JSON.parse(response.body, symbolize_names: true)
     end
   end
 end
