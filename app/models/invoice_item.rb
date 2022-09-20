aa81185991c2abe79fdaa62db1194de7de09406b
Thomas Hayes <hayes.thomas610@gmail.com>
2022-09-19 18:46:55 -0600

    model tests for .for_merchant class method

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -10,7 +10,7 @@ class InvoiceItem < ApplicationRecord
   enum status: [ :pending, :packaged, :shipped]
 
   def self.for_merchant(merchant_id)
-    select(:item_id, :status, :unit_price, :quantity)
+    select("invoice_items.item_id, invoice_items.status, invoice_items.unit_price, invoice_items.quantity")
       .joins(item: [:invoice_items])
       .where("items.merchant_id = #{merchant_id}")
   end
