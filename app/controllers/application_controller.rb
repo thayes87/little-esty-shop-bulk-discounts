5a45343b19dfad8bf0b4a615264c3fd9cf3b3c6f
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-09-20 14:41:32 -0600

    Add github repo name to footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
+require_relative '../facades/github_facade'
+
 class ApplicationController < ActionController::Base
+  before_action :github
+
+  def github
+    data = GitHubFacade.github_info
+    @github_name = data.name
+  end
 end
--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -4,6 +4,6 @@ require 'json'
 class GitHubService
   def self.request
     response = HTTParty.get('https://api.github.com/repos/Dominicod/little-esty-shop')
-    json_response = JSON.parse(response.body,symbolize_names: true)
+    json_response = JSON.parse(response.body, symbolize_names: true)
   end
 end
