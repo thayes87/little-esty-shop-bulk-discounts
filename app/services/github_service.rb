c75e98955aa67bbe6c760b47da188174f6f277cb
Thomas Hayes <hayes.thomas610@gmail.com>
2022-09-20 15:31:27 -0600

    add figaro gem and YML file; create dynamic path in service

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -2,8 +2,13 @@ require 'httparty'
 require 'json'
 
 class GitHubService
-  def self.request
-    response = HTTParty.get('https://api.github.com/repos/Dominicod/little-esty-shop')
-    json_response = JSON.parse(response.body, symbolize_names: true)
+  def self.request(path, auth_required = false)
+    if auth_required
+      response = HTTParty.get("https://api.github.com/repos/Dominicod/little-esty-shop#{path}", headers: {authorization: "Bearer " + ENV['token']})
+      json_response = JSON.parse(response.body, symbolize_names: true)
+    else
+      response = HTTParty.get("https://api.github.com/repos/Dominicod/little-esty-shop#{path}")
+      json_response = JSON.parse(response.body, symbolize_names: true)
+    end
   end
 end
