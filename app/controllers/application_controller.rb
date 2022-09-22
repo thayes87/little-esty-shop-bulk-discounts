d72945d5746338d1212b17a96fc8589b388f7478
Thomas Hayes <hayes.thomas610@gmail.com>
2022-09-22 13:40:41 -0600

    delete github API info from project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
-  before_action :github
-
-  def github
-    @github_name = GithubFacade.github_info.name
-    @github_logins = GithubFacade.github_info("/collaborators", true)
-    @github_pulls = GithubFacade.github_info("/pulls?state=closed&per_page=100").count
-  end
+  
 end
