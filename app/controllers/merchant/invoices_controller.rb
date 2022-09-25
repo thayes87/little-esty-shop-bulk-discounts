4d4690e45e8318b79f0164efcaaf9b4c0cfe7888
Thomas Hayes <hayes.thomas610@gmail.com>
2022-09-24 18:56:41 -0600

    WIP - User story 5: Total Revenue and Discounted Revenue

--- app/controllers/merchant/invoices_controller.rb
+++ app/controllers/merchant/invoices_controller.rb
@@ -9,6 +9,20 @@ class Merchant::InvoicesController < ApplicationController
     @invoice = Invoice.find(params[:id])
     @merchant = Merchant.find(params[:merchant_id])
     @invoice_items = @invoice.invoice_items
+
+    
+      
+    if @merchant.bulk_discounts.count == 1 && @invoice_items.where("quantity >= ?", @merchant.bulk_discounts.first.quantity_break).present?
+      discounted_items = @invoice_items.where("quantity >= ?", @merchant.bulk_discounts.first.quantity_break)
+      non_discounted_items = @invoice_items.where("quantity < ?", @merchant.bulk_discounts.first.quantity_break)
+
+      discount_revenue = discounted_items.sum('quantity * unit_price') * (1 - (@merchant.bulk_discounts.first.discount.to_f / 100))
+      non_discounted_revenue = non_discounted_items.sum('quantity * unit_price').to_f
+
+      @bulk_discount_revenue = discount_revenue + non_discounted_revenue
+    else 
+      @bulk_discount_revenue = @invoice.total_revenue
+    end
   end
 
   # def update
--- spec/features/merchant/invoices/show_spec.rb
+++ spec/features/merchant/invoices/show_spec.rb
@@ -122,5 +122,69 @@ RSpec.describe 'Merchant Invoices Show Page' do
         end
       end
     end
+    
+    describe 'Merchant Invoice Show Page: Total Revenue and Discounted Revenue' do
+      describe 'When I visit my merchant invoice show page, ' do
+        it 'I see the total revenue for my merchant from this invoice (not including discounts)' do
+          @merchant1 = Merchant.create!(id: 45, name:"Bob's Baskets")
+
+          @discount_a = BulkDiscount.create!(description: "A", quantity_break: 10, discount: 25, merchant_id: @merchant1.id)
+
+          @customer1 = Customer.create!(id: 45, first_name:"John", last_name:"Doe")
+
+          @item1 = Item.create!(id: 45, name:"Big basket", description:"Green and big", unit_price: 1499, merchant_id: @merchant1.id)
+          @item2 = Item.create!(id: 46, name:"Medium basket", description:"Blue and medium", unit_price: 1399, merchant_id: @merchant1.id)
+
+          @invoice1 = Invoice.create!(id: 45, customer_id: @customer1.id, status: 1)
+
+          @invoice_item1 = InvoiceItem.create!(id: 45, item_id: @item1.id, invoice_id: @invoice1.id, quantity:5, unit_price:1499 , status: 0)
+          @invoice_item2 = InvoiceItem.create!(id: 46, item_id: @item2.id, invoice_id: @invoice1.id, quantity:10 , unit_price:1399 , status: 1)
+          
+          visit merchant_invoice_path(@merchant1, @invoice1)
+          
+          expect(page).to have_content("Total Revenue: $214.85")
+        end
+
+        it 'I see the total discounted revenue for my merchant from this invoice which when the merchant does NOT have any current discounts' do
+          @merchant1 = Merchant.create!(id: 45, name:"Bob's Baskets")
+          
+          @customer1 = Customer.create!(id: 45, first_name:"John", last_name:"Doe")
+
+          @item1 = Item.create!(id: 45, name:"Big basket", description:"Green and big", unit_price: 1499, merchant_id: @merchant1.id)
+          @item2 = Item.create!(id: 46, name:"Medium basket", description:"Blue and medium", unit_price: 1399, merchant_id: @merchant1.id)
+
+          @invoice1 = Invoice.create!(id: 45, customer_id: @customer1.id, status: 1)
+
+          @invoice_item1 = InvoiceItem.create!(id: 45, item_id: @item1.id, invoice_id: @invoice1.id, quantity:5, unit_price:1499 , status: 0)
+          @invoice_item2 = InvoiceItem.create!(id: 46, item_id: @item2.id, invoice_id: @invoice1.id, quantity:10 , unit_price:1399 , status: 1)
+          
+          visit merchant_invoice_path(@merchant1, @invoice1)
+
+          expect(page).to have_content("Total Discounted Revenue: $214.85")
+        end
+        
+        it 'I see the total discounted revenue for my merchant from this invoice which when the merchant has ONE discount' do
+          @merchant1 = Merchant.create!(id: 45, name:"Bob's Baskets")
+          
+          @discount_a = BulkDiscount.create!(description: "A", quantity_break: 10, discount: 25, merchant_id: @merchant1.id)
+
+          @customer1 = Customer.create!(id: 45, first_name:"John", last_name:"Doe")
+
+          @item1 = Item.create!(id: 45, name:"Big basket", description:"Green and big", unit_price: 1499, merchant_id: @merchant1.id)
+          @item2 = Item.create!(id: 46, name:"Medium basket", description:"Blue and medium", unit_price: 1399, merchant_id: @merchant1.id)
+          @item3 = Item.create!(id: 47, name:"Mini basket", description:"pink and small", unit_price: 999, merchant_id: @merchant1.id)
+
+          @invoice1 = Invoice.create!(id: 45, customer_id: @customer1.id, status: 1)
+
+          @invoice_item1 = InvoiceItem.create!(id: 45, item_id: @item1.id, invoice_id: @invoice1.id, quantity:5, unit_price:1499 , status: 0)
+          @invoice_item2 = InvoiceItem.create!(id: 46, item_id: @item2.id, invoice_id: @invoice1.id, quantity:10 , unit_price:1399 , status: 1)
+          @invoice_item3 = InvoiceItem.create!(id: 47, item_id: @item3.id, invoice_id: @invoice1.id, quantity:10 , unit_price:999 , status: 1)
+          
+          visit merchant_invoice_path(@merchant1, @invoice1)
+
+          expect(page).to have_content("Total Discounted Revenue: $254.80")
+        end
+      end
+    end
   end
 end
