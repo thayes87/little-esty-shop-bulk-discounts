f9464025d525cb3ed12802582bf09bf15819fe19
Thomas Hayes <hayes.thomas610@gmail.com>
2022-09-26 16:56:03 -0600

    WIP user story 6: Discounted Revenue

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,5 +1,6 @@
 class Invoice < ApplicationRecord
   belongs_to :customer
+  has_many :bulk_discounts, through: :merchant
   has_many :invoice_items
   has_many :items, through: :invoice_items
   has_many :transactions
